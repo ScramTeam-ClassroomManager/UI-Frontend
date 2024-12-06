87b125053d9aab592e0c214f918e11df22d1e8e1
serrux <srrfnc02b19d122v@studenti.unical.it>
2024-12-06 20:17:45 +0100

    Creato EventHandler per aggiungere e rimuovere prenotazioni in maniera persistente direttamente dal calendario

--- src/main/java/it/unical/classroommanager_ui/controller/CalendarController.java
+++ src/main/java/it/unical/classroommanager_ui/controller/CalendarController.java
@@ -5,19 +5,23 @@ import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.unical.classroommanager_ui.model.CustomEntryDetailsView;
-import it.unical.classroommanager_ui.model.RequestDto;
-import it.unical.classroommanager_ui.model.User;
-import it.unical.classroommanager_ui.model.UserManager;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import it.unical.classroommanager_ui.model.*;
 import javafx.fxml.FXML;
 import com.calendarfx.view.CalendarView;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,11 +39,18 @@ public class CalendarController {
 
     private List<RequestDto> requests = new ArrayList<>();
 
-    private void addEvent() {
-        // Aggiungi un evento al calendario
+    private void addAllEvent() {
+
+        try{
+            requests = getAllRequests();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
         for (RequestDto request : requests) {
             try {
-                Entry entry = new Entry();
+                Entry<RequestDto> entry = new Entry<>();
                 if (request.getStatus().equals("ACCEPTED")) {
                     entry.changeStartDate(request.getRequestDate());
                     entry.changeStartTime(request.getStartHour());
@@ -49,7 +60,8 @@ public class CalendarController {
                     entry.setTitle(getUserName(request.getUserSerialNumber()) + ": "
                             + request.getReason() + "\n"
                             + "Aula: " + entry.getLocation() + "\n");
-                    calendarView.getCalendars().getFirst().addEntries(entry);
+                    entry.setUserObject(request);
+                    calendar.addEntries(entry);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -57,26 +69,105 @@ public class CalendarController {
         }
     }
 
-    private void createRequest(){
-        try{
-            requests = getAllRequests();
+
+    private Entry createEntry(){
+        Entry<RequestDto> entry = new Entry<>();
+
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Inserisci Nuova Richiesta");
+        dialog.setHeaderText("Inserisci i dettagli dell'evento");
+
+        MFXTextField titleField = new MFXTextField();
+        titleField.setPromptText("Motivazione");
+        titleField.setPrefWidth(160);
+
+        MFXTextField locationField = new MFXTextField();
+        locationField.setPromptText("Aula");
+        locationField.setPrefWidth(160);
+
+        MFXDatePicker datePicker = new MFXDatePicker();
+        MFXComboBox<LocalTime> startHourCB = new MFXComboBox<>();
+        MFXComboBox<LocalTime> endHourCB = new MFXComboBox<>();
+
+        for (int i = 8; i < 21; i++){
+            startHourCB.getItems().add(LocalTime.of(i,0));
+            startHourCB.getItems().add(LocalTime.of(i, 30));
+
+            endHourCB.getItems().add(LocalTime.of(i,0));
+            endHourCB.getItems().add(LocalTime.of(i, 30));
         }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.add(new Label("Motivazione:"), 0, 0);
+        grid.add(titleField, 1, 0);
+        grid.add(new Label("Aula:"), 0, 1);
+        grid.add(locationField, 1, 1);
+        grid.add(new Label("Data:"), 0, 2);
+        grid.add(datePicker, 1, 2);
+        grid.add(new Label("Ora Inizio:"), 0, 3);
+        grid.add(startHourCB, 1, 3);
+        grid.add(new Label("Ora Fine:"), 0, 4);
+        grid.add(endHourCB, 1, 4);
+
+        dialog.getDialogPane().setContent(grid);
+
+
+        ButtonType createButtonType = new ButtonType("Crea", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(createButtonType, ButtonType.CANCEL);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == createButtonType) {
+                return new Pair<>(titleField.getText(), locationField.getText());
+            }
+            return null;
+        });
+
+        dialog.showAndWait().ifPresent(result -> {
+            entry.setTitle(result.getKey());
+            entry.setLocation(result.getValue());
+            entry.changeStartDate(datePicker.getValue());
+            entry.changeStartTime(startHourCB.getValue());
+            entry.changeEndDate(datePicker.getValue());
+            entry.changeEndTime(endHourCB.getValue());
+        });
+
+        return entry;
     }
 
 
     @FXML
     public void init(MainPageController mainPageController) {
-        // Configura il calendario (opzionale)
-        this.mainPageController = mainPageController;
 
+        this.mainPageController = mainPageController;
+        addAllEvent();
+        calendar.addEventHandler(new CustomCalendarEventHandler());
+        if (UserManager.getInstance().getToken().isEmpty() || !(UserManager.getInstance().getUser().role().equals("ADMIN"))) {
+            calendar.setReadOnly(true);
+        }
         calendarView.setEntryDetailsPopOverContentCallback(param -> new CustomEntryDetailsView(param.getEntry()));
+
+        calendarView.setContextMenuCallback(param -> {
+            if (UserManager.getInstance().getToken().isEmpty())
+                return new ContextMenu();
+
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem addEntry = new MenuItem("Aggiungi Richiesta");
+            addEntry.setOnAction(e -> {
+
+                Entry<RequestDto> entry = createEntry();
+                entry.setCalendar(calendar);
+                calendar.addEntry(entry);
+            });
+
+            contextMenu.getItems().add(addEntry);
+            return contextMenu;
+        });
+
         calendarSource.getCalendars().add(calendar);
         calendarView.getCalendarSources().add(calendarSource);
-        createRequest();
-        addEvent();
 
         calendarView.showDayPage();
     }
